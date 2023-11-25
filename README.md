582a0e4c4fb1e6b336ff291d22957d83579599eb
Kirill Lykov <kirill.lykov@solana.com>
2023-11-25 13:42:19 +0000

    add readme file

---
+++ README.md
@@ -0,0 +1,15 @@
+### Workshop solana program example
+
+Some adoptation of https://github.com/solana-labs/example-helloworld
+
+### Build and test for program compiled natively
+```
+$ cargo build
+$ cargo test
+```
+
+### Build and test the program compiled for BPF
+```
+$ cargo build-bpf
+$ cargo test-bpf
+```
--- workshop-program/src/lib.rs
+++ workshop-program/src/lib.rs
@@ -11,20 +11,8 @@ use {
 };
 
 // Enum describing possible entry point instructions
-#[derive(Clone, Debug, BorshSerialize, BorshDeserialize, PartialEq)]
-pub enum WorkshopProgramInstruction {
-    Add {
-        // value to add
-        increment: u64,
-    },
-}
 
 /// Define the type of state stored in accounts
-#[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct WorkshopAccountData {
-    pub value: u64,
-}
-
 // Declare and export the program's entrypoint
 entrypoint!(process_instruction);
 
@@ -35,32 +23,6 @@ pub fn process_instruction(
     instruction_data: &[u8],
 ) -> ProgramResult {
     msg!("Process instruction");
-
-    let instruction = WorkshopProgramInstruction::try_from_slice(instruction_data)?;
-
-    let accounts_iter = &mut accounts.iter();
-    let account = next_account_info(accounts_iter)?;
-
-    // The account must be owned by the program in order to modify its data
-    if account.owner != program_id {
-        msg!("Greeted account does not have the correct program id");
-        return Err(ProgramError::IncorrectProgramId);
-    }
-
-    match instruction {
-        WorkshopProgramInstruction::Add { increment } => add_calc(account, increment),
-    }
-}
-
-fn add_calc(account: &AccountInfo, increment: u64) -> ProgramResult {
-    msg!("Add");
-
-    let mut account_data = WorkshopAccountData::try_from_slice(&account.data.borrow())?;
-    account_data.value += increment;
-    account_data.serialize(&mut &mut account.data.borrow_mut()[..])?;
-
-    msg!("Data = {}", account_data.value);
-    Ok(())
 }
 
 // Sanity tests
